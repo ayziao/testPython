f7966b29e6ef26a6cf292877d948405a7e621ddb
ayziao <ayziao+git@gmail.com>
2013-09-28 08:36:00 +0900

    ファイル名大文字小文字の変更がコミット漏れする

---
+++ myApp/MyAppaaa.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+
+class MyApp:
+	"""
+		オレオレアプリ
+	"""
+
+	def __init__(self):
+		pass
+
+
+	@classmethod
+	def main(cls):
+		"""
+		hogehoge
+		"""
+		print 'main'
+		return True
+
+
+if __name__ == '__main__':
+	instance = MyApp()
+	instance.main()
+
